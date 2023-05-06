c5f2f74146482aa4796be97c5a3ffd4c7fbd499e
William Beukelman <williambeukelman@my.cwi.edu>
2023-05-06 16:43:31 -0600

    Added screenshot relative urls to store

--- bundle.js
+++ bundle.js
@@ -924,7 +924,7 @@ var app = (function () {
         demo: "https://v62igf.csb.app/artist",
         code: "https://github.com/williambeukelman/Music-Library-App",
         image:
-          "https://camo.githubusercontent.com/ad0810c98f921ee6571c2274ea4398eb022877d97642ea0b974dd482b16db718/68747470733a2f2f77696c6c69616d6265756b656c6d616e2e6769746875622e696f2f6d757369632d706c617965722d616c62756d732e706e67",
+          "public/screenshots/music-player-albums.png",
         badges: ["Vue", "Bootstrap"]
       },
       {
@@ -933,7 +933,7 @@ var app = (function () {
           "A mock business website I designed and coded using Bootstrap in a parallax style.",
         demo: "https://owv4in.csb.app/",
         code: "https://github.com/williambeukelman/Recyclical-Energy-Website/",
-        image: "https://williambeukelman.github.io/energy-home.png",
+        image: "public/screenshots/energy-home.png",
         badges: ["Bootstrap"]
       },
       {
@@ -943,7 +943,7 @@ var app = (function () {
         demo: "https://s9o1y6.csb.app/",
         code: "https://github.com/williambeukelman/Twenty-Letter-Wordle/",
         image:
-          "https://williambeukelman.github.io/screenshot-twenty-letter-wordle.png",
+          "public/screenshots/screenshot-twenty-letter-wordle.png",
         badges: ["Svelte"]
       },
       {
@@ -951,14 +951,14 @@ var app = (function () {
         desc: "A fictional nursery website made in javascript and jquery.",
         demo: "https://rgx9lb.csb.app/",
         code: "https://codesandbox.io/s/rgx9lb",
-        image: "https://williambeukelman.github.io/screenshot-nursery-website.png",
+        image: "public/screenshots/screenshot-nursery-website.png",
         badges: ["Javascript", "Jquery"]
       },
       {
         title: "Bike Shop App - Capstone Project",
         desc: "A multiplatform web app, storefront, and ecommerce adminstration tool built together with 9 other students as a capstone class.",
         demo: "https://thebikeshop.app",
-        code: "https://github.com/CWI-SWDV-280-Bike-Shop/Bike-Shop",
+        code: "https://github.com/CWI-SWDV-280-Bike-Shop/Bike-Shop/tree/main",
         image: "public/screenshots/screenshot_thebikeshop.png",
         badges: ["React-Native", "Mongodb", "Javascript", "ExpressJS", "Docker", "NodeJS"]
       },
